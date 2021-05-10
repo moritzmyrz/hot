587c0923aff053e59d296c02eba22b0e73074fbc
1IM <1im@hedmark.org>
2021-05-10 14:33:09 +0200

    Simple Header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,26 @@
-import '../styles/globals.css'
+import '../style/fonts.css';
+import 'tailwindcss/tailwind.css';
+
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
+import React from 'react';
+
+import Header from '../components/Header';
+
+const theme = createMuiTheme({});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <React.Fragment>
+      <ThemeProvider theme={theme}>
+        <div className="flex flex-col h-screen bg-gray-200">
+          <Header />
+          <div className="flex-1">
+            <Component {...pageProps} />
+          </div>
+        </div>
+      </ThemeProvider>
+    </React.Fragment>
+  );
 }
 
-export default MyApp
+export default MyApp;
