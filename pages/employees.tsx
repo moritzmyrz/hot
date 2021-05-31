24bb3f971faa2fb017c8303e81ea72f689e98270
Moritz Myrseth <moritzmyrz@gmail.com>
2021-05-31 10:19:38 +0200

    employees

---
+++ pages/employees.tsx
@@ -0,0 +1,48 @@
+import { Avatar } from "@material-ui/core";
+import React from "react";
+
+interface Employee {
+	avatar: string;
+	name: string;
+	title: string;
+}
+
+const employees: Employee[] = [
+	{
+		avatar: "/avatars/1.png",
+		name: "Johan Gunnarsen",
+		title: "Daglig Leder",
+	},
+	{
+		avatar: "/avatars/2.png",
+		name: "Silje Torgersen",
+		title: "Journalist",
+	},
+	{
+		avatar: "/avatars/3.png",
+		name: "Rolf Andersen",
+		title: "Journalist & Vaskepersonell",
+	},
+];
+
+const Employees = () => {
+	return (
+		<div className="grid grid-cols-2 gap-2 m-auto w-2/4">
+			{employees.map((emp) => (
+				<div className="bg-gray-100 rounded-lg p-2 flex h-full">
+					<Avatar
+						alt={emp.name}
+						src={emp.avatar}
+						style={{ height: 64, width: 64 }}
+					/>
+					<div className="my-auto flex flex-col">
+						<p className="font-bold">{emp.name}</p>
+						<p>{emp.title}</p>
+					</div>
+				</div>
+			))}
+		</div>
+	);
+};
+
+export default Employees;
